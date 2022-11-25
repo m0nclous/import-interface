3f05ada1956eb066c78bc95b316a900d8dae2824
m0nclous <m0nclous@vk.com>
2022-11-25 18:14:54 +0600

    Базовый макет

--- src/vue/App.vue
+++ src/vue/App.vue
@@ -1,5 +1,9 @@
 <template>
-
+    <el-container>
+        <el-header>Header</el-header>
+        <el-main>Main</el-main>
+        <el-footer>Footer</el-footer>
+    </el-container>
 </template>
 
 <script setup>
@@ -7,5 +11,26 @@
 </script>
 
 <style scoped>
+    .el-container {
+        margin: auto;
+        max-width: 1320px;
+        box-shadow: var(--el-box-shadow-dark);
+        border-radius: var(--el-border-radius-base);
+    }
 
+    .el-header {
+        --el-header-padding: 20px;
+    }
+
+    .el-footer {
+        --el-footer-padding: 20px;
+    }
+</style>
+
+<style>
+    body {
+        display: flex;
+        height: 100vh;
+        margin: 0;
+    }
 </style>
