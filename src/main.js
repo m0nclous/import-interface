c7eadc9d995d82662ebdeb319a4f5fbdb923765e
m0nclous <m0nclous@vk.com>
2022-11-25 16:54:00 +0600

    Добавлен Element-Plus

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue';
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
 import App from './vue/App.vue';
 
-createApp(App).mount('body');
+createApp(App).use(ElementPlus).mount('body');
