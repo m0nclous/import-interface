230dd3ac822562ccc6e85c9be503619302661cc6
m0nclous <m0nclous@vk.com>
2022-12-06 00:51:15 +0600

    Добавлены информационные alert и демонстрационные данные

--- src/vue/App.vue
+++ src/vue/App.vue
@@ -1,7 +1,7 @@
 <template>
     <el-container>
         <el-header>
-            <el-steps :active="null" finish-status="success" align-center>
+            <el-steps :active="2" finish-status="success" align-center>
                 <el-step title="Категории" description="Импорт категорий" />
                 <el-step title="Изображения" description="Импорт изображений" />
                 <el-step title="Товары" description="Импорт товаров" />
@@ -12,11 +12,27 @@
 
         <el-main>
             <el-row>
-                <el-col></el-col>
+                <el-col :span="6" :offset="4">
+                    <el-row>
+                        <el-col>
+                            <el-alert title="Импорт категорий" type="success" :closable="false">
+                                <div>79 / 79 обработано</div>
+                                <el-progress :percentage="100" :show-text="false" />
+                            </el-alert>
+                        </el-col>
 
-                <el-col>
+                        <el-col>
+                            <el-alert title="Импорт изображений" type="success" :closable="false">
+                                <div>300 / 300 обработано</div>
+                                <el-progress :percentage="100" :show-text="false" />
+                            </el-alert>
+                        </el-col>
+                    </el-row>
+                </el-col>
+
+                <el-col :span="5">
                     <el-progress type="circle" :percentage="0">
-                        <el-button type="success" size="large" circle>
+                        <el-button type="danger" size="large" circle>
                             <el-icon size="28">
                                 <SwitchButton />
                             </el-icon>
@@ -24,7 +40,29 @@
                     </el-progress>
                 </el-col>
 
-                <el-col></el-col>
+                <el-col :span="6">
+                    <el-row>
+                        <el-col>
+                            <el-alert title="Импорт товаров" type="process" :closable="false">
+                                <div>25 000 / 50 000 позиций</div>
+                                <el-progress :percentage="50" :show-text="false" />
+                            </el-alert>
+                        </el-col>
+
+                        <el-col>
+                            <el-alert title="Импорт вариаций" type="info" :closable="false">
+                                <div>0 / 50 000 позиций</div>
+                                <el-progress :percentage="0" :show-text="false" />
+                            </el-alert>
+                        </el-col>
+                    </el-row>
+                </el-col>
+            </el-row>
+
+            <el-row style="text-align: center; margin-top: 40px;">
+                <el-col>Последняя синхронизация 24.11.2022 20:48</el-col>
+                <el-col><b>import0_1.xml</b> обновлён 05.12.2022 16:25</el-col>
+                <el-col><b>offers0_1.xml</b> обновлён 05.12.2022 16:25</el-col>
             </el-row>
         </el-main>
 
@@ -62,11 +100,11 @@
         color: var(--el-text-color-primary);
     }
 
-    .el-col:nth-child(2) {
+    .el-footer .el-col:nth-child(2) {
         text-align: center;
     }
 
-    .el-col:nth-child(3) {
+    .el-footer .el-col:nth-child(3) {
         text-align: right;
     }
 
@@ -83,6 +121,40 @@
         display: flex;
         align-items: center;
         justify-content: center;
+        flex-direction: column;
+    }
+
+    .el-main > .el-row {
+        width: 100%;
+    }
+
+    .el-main .el-row {
+        row-gap: 20px;
+    }
+
+    .el-main > .el-row > .el-col:nth-child(2) {
+        text-align: center;
+        align-self: center;
+    }
+
+    .el-alert:deep(.el-alert__content) {
+        display: flex;
+        flex-direction: column;
+        width: 100%;
+    }
+
+    .el-alert .el-progress {
+        padding: 7px 0;
+    }
+
+    .el-alert--process {
+        --el-alert-bg-color: var(--el-color-primary-light-9);
+        background-color: var(--el-alert-bg-color);
+        color: var(--el-color-info);
+    }
+
+    .el-alert--success:deep(.el-progress-bar__inner) {
+        background-color: var(--el-color-success);
     }
 
     .el-button--large {
