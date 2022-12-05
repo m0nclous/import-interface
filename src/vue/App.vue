dcb6c0a60c591be0e7be86ad32b56542a27a7cfe
m0nclous <m0nclous@vk.com>
2022-12-05 23:46:31 +0600

    Добавлен footer

--- src/vue/App.vue
+++ src/vue/App.vue
@@ -2,7 +2,23 @@
     <el-container>
         <el-header>Header</el-header>
         <el-main>Main</el-main>
-        <el-footer>Footer</el-footer>
+
+        <el-footer>
+            <el-row>
+                <el-col :span="8">
+                    Powered by
+                    <el-link type="primary">m0nclous</el-link>
+                </el-col>
+
+                <el-col :span="8">
+                    <el-tag>Seed 018f6062-97c2-4be2-8edf-41beb8e0b275</el-tag>
+                </el-col>
+
+                <el-col :span="8">
+                    v1.0.0
+                </el-col>
+            </el-row>
+        </el-footer>
     </el-container>
 </template>
 
@@ -14,8 +30,18 @@
     .el-container {
         margin: auto;
         max-width: 1320px;
+        min-height: 700px;
         box-shadow: var(--el-box-shadow-dark);
         border-radius: var(--el-border-radius-base);
+        color: var(--el-text-color-primary);
+    }
+
+    .el-col:nth-child(2) {
+        text-align: center;
+    }
+
+    .el-col:nth-child(3) {
+        text-align: right;
     }
 
     .el-header {
@@ -28,6 +54,10 @@
 </style>
 
 <style>
+    html {
+        font-size: 12px;
+    }
+
     body {
         display: flex;
         height: 100vh;
